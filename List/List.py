2315f6fd51d9d271c122a4255a4b04239218568d
AnthonyJo <joseongkwang@gmail.com>
2021-01-01 14:09:24 +0900

    Start Python

---
+++ List/List.py
@@ -0,0 +1,14 @@
+mobileCarrier = ['SKT', 'KT', 'LG'] 
+
+print("len(mobileCarrier) =", len(mobileCarrier))
+
+print("mobileCarrier[0] =", mobileCarrier[0])
+
+mobileCarrier.append('KDDI')
+mobileCarrier.append('NTT')
+
+print("mobileCarrier =", mobileCarrier)
+
+mobileCarrier.remove('KT')
+
+print("mobileCarrier =", mobileCarrier)
---
+++ Variable/Variable.py
@@ -0,0 +1,13 @@
+one = 1
+two = 2
+
+result = one + two
+
+print(result)
+
+rate = 0.85
+price = 5000
+
+result = price * rate
+
+print("result = ", result)
